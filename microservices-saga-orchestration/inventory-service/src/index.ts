99219e42f1c4f637ff1cd921f20700e3fe0a46ce
Ritesh Bamola <rishubamola01@gmail.com>
2026-02-15 10:58:44 +0530

    feat(inventory-service): setup Kafka infrastructure and verify message flow via handshake topic

--- microservices-saga-orchestration/inventory-service/src/index.ts
+++ microservices-saga-orchestration/inventory-service/src/index.ts
@@ -1,8 +1,22 @@
 import { pool, connectDB } from "./db";
-
+import { connectKafka, producer } from "./kafka";
 const startServer = async () => {
   await connectDB();
   const db = await pool.query(`SELECT current_database();`);
+
+  await connectKafka();
+  console.log("Kafka fully connected");
+  await producer.send({
+    topic: "inventory.handshake",
+    messages: [
+      {
+        key: "234",
+        value: "Hello Kafka!",
+      },
+    ],
+  });
+
+  console.log("OK");
 };
 
 startServer();
---
+++ microservices-saga-orchestration/inventory-service/src/kafka.ts
@@ -0,0 +1,77 @@
+import { Kafka } from "kafkajs";
+
+const kafka = new Kafka({
+  clientId: "inventory-service",
+  brokers: ["localhost:9092"],
+});
+
+const admin = kafka.admin();
+
+const createTopics = async () => {
+  await admin.connect();
+
+  await admin.createTopics({
+    topics: [
+      { topic: "inventory.reserve", numPartitions: 3, replicationFactor: 1 },
+      { topic: "inventory.release", numPartitions: 3, replicationFactor: 1 },
+      { topic: "inventory.reserved", numPartitions: 3, replicationFactor: 1 },
+      { topic: "inventory.released", numPartitions: 3, replicationFactor: 1 },
+      { topic: "inventory.failed", numPartitions: 3, replicationFactor: 1 },
+      { topic: "inventory.handshake", numPartitions: 1, replicationFactor: 1 },
+    ],
+    waitForLeaders: true,
+  });
+
+  await admin.disconnect();
+};
+
+export const producer = kafka.producer({
+  allowAutoTopicCreation: false,
+});
+
+export const consumer = kafka.consumer({
+  groupId: "inventory-service-group",
+});
+
+export const connectKafka = async () => {
+  await createTopics();
+  await producer.connect();
+  await consumer.connect();
+
+  await consumer.subscribe({
+    topics: [
+      "inventory.reserve",
+      "inventory.release",
+      "inventory.reserved",
+      "inventory.released",
+      "inventory.failed",
+      "inventory.handshake",
+    ],
+    fromBeginning: false,
+  });
+
+  await consumer.run({
+    autoCommit: false,
+    eachMessage: async ({ topic, partition, message }) => {
+      try {
+        switch (topic) {
+          case "inventory.handshake":
+            console.log(
+              `Received message from ${topic}: ${message.value?.toString()}`,
+            );
+            break;
+        }
+
+        await consumer.commitOffsets([
+          {
+            topic,
+            partition,
+            offset: (Number(message.offset) + 1).toString(),
+          },
+        ]);
+      } catch (error) {
+        console.error("Kafka Processing Error:", error);
+      }
+    },
+  });
+};
